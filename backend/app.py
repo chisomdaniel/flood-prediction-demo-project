c32155d3bb58daeb5aca94f632414c778ee95c74
chisomdaniel <chinwezechisom@gmail.com>
2025-02-17 18:49:49 +0100

    fixed error

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 #!/bin/env python3
 from flask import Flask, jsonify
 from flask_cors import CORS
-from .dummy import generate_dummy_forecast
+from dummy import generate_dummy_forecast
 
 app = Flask(__name__)
 CORS(app)
@@ -11,7 +11,7 @@ def home():
     return "Welcome to the flood prediction API.\
         Access the endpoint '/api/forecast/< str:city >/< int:days >', where 'days' in the number of future days you want to get a prediction for (e.g. /api/lagos/23)"
 
-@app.route("/api/forecast/<str:city>/<int:days>", methods=['GET', 'POST'], strict_slashes=False)
+@app.route("/api/forecast/<city>/<int:days>", methods=['GET', 'POST'], strict_slashes=False)
 def predict(city='lagos', days='5'):
     """Get prediction"""
     return jsonify(generate_dummy_forecast(city, days))
